168ddb66845c80612f460856a98481def01957c2
Emilson de Souza Nascimento <emilsonsn2@gmail.com>
2026-02-15 20:37:41 -0300

    feat: traduz tela de posts e adiciona coluna destaque

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-            <h6 class="card-title float-start">Post List</h6>
+            <h6 class="card-title float-start">Lista de Posts</h6>
             <div class="card-title float-end btn btn-primary btn-sm">
-                <a href="{{ route('admin.posts.create') }}" class="text-white">Add New</a>
+                <a href="{{ route('admin.posts.create') }}" class="text-white">Adicionar Novo</a>
             </div>
         </div>
         <div class="card-body">
@@ -14,13 +14,14 @@
                     <thead>
                         <tr>
                             <th style="width: 10px">#</th>
-                            <th>Title</th>
+                            <th>Título</th>
                             <th>Thumbnail</th>
-                            <th>Category</th>
-                            <th>Description</th>
-                            <th>CreatedBy</th>
-                            <th>Created At</th>
-                            <th style="width: 40px">Action</th>
+                            <th>Categoria</th>
+                            <th>Descrição</th>
+                            <th>Criado Por</th>
+                            <th>Destaque</th>
+                            <th>Criado em</th>
+                            <th style="width: 40px">Ações</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -28,15 +29,18 @@
                             <tr>
                                 <td>{{ $post->id }}</td>
                                 <td>{{ $post->title }}</td>
-                                <td><img src="{{ $post->thumbnail ? asset('storage/uploads/posts/' . $post->thumbnail) : asset('images/placeholder-post.png') }}"
-                                        style="width: 50px; height: 50px"></td>
+                                <td>
+                                    <img src="{{ $post->thumbnail ? asset('storage/uploads/posts/' . $post->thumbnail) : asset('images/placeholder-post.png') }}"
+                                        style="width: 50px; height: 50px">
+                                </td>
                                 <td>{{ $post->category->title }}</td>
                                 <td>{{ Str::limit(strip_tags($post->body), 40) }}</td>
                                 <td>{{ optional($post->user)->name }}</td>
+                                <td>{{ $post->is_highlighted ? 'Sim' : 'Não' }}</td>
                                 <td>{{ $post->created_at->diffForHumans() }}</td>
                                 <td>
                                     <a href="{{ route('admin.posts.edit', $post->id) }}">
-                                        <span class="badge bg-primary">Edit</span>
+                                        <span class="badge bg-primary">Editar</span>
                                     </a>
                                     <form id="delete-form-{{ $post->id }}" method="post"
                                         action="{{ route('admin.posts.destroy', $post->id) }}" style="display: none">
@@ -45,16 +49,16 @@
                                     </form>
                                     <a href="javascript:void(0)" class="badge bg-danger text-white"
                                         onclick="
-                                    if(confirm('Are you sure, You want to Delete this ??'))
+                                    if(confirm('Tem certeza que deseja excluir?'))
                                     {
                                     event.preventDefault();
                                     document.getElementById('delete-form-{{ $post->id }}').submit();
-                                    }">Delete
+                                    }">Excluir
                                     </a>
                                 </td>
                             </tr>
                         @empty
-                            <td colspan="7" class="text-center">No data found!</td>
+                            <td colspan="9" class="text-center">Nenhum registro encontrado!</td>
                         @endforelse
                     </tbody>
                 </table>
